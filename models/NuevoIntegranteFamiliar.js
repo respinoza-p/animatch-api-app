a954192d8e965f83c59aa2a2abc1f7ec29e0d4f6
caudillodev <espinozaplaza.rodrigo@gmail.com>
2025-02-16 22:32:58 -0300

    Se incrementan los caracteres para nuevoIntegrante

--- models/NuevoIntegranteFamiliar.js
+++ models/NuevoIntegranteFamiliar.js
@@ -6,7 +6,7 @@ const nuevoIntegranteFamiliarSchema = new mongoose.Schema(
       type: String,
       required: [true, "El valor es obligatorio"],
       minlength: [2, "El valor debe tener al menos 2 caracteres"],
-      maxlength: [90, "El valor no puede exceder los 90 caracteres"],
+      maxlength: [150, "El valor no puede exceder los 150 caracteres"],
       unique: true
     },
     estado: {
